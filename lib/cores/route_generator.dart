f8c979b88bfe10dab1e38a4531933f0af2f5c73a
Ahmad Rifa'i <arifai209@gmail.com>
2022-04-11 22:22:52 +0700

    Move route_generator.dart to cores folder

---
+++ lib/cores/route_generator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:morph_wallet/blocs/morph/morph_bloc.dart';
+import 'package:morph_wallet/cores/morph_core.dart';
+import 'package:morph_wallet/screens/create_wallet/create_wallet.dart';
+import 'package:morph_wallet/screens/create_wallet/wallet_info_form_screen.dart';
+import 'package:morph_wallet/screens/empty/empty_screen.dart';
+import 'package:morph_wallet/screens/main/main_screen.dart';
+import 'package:morph_wallet/screens/onboarding/onboarding_screen.dart';
+
+class RouteGenerator {
+  final RouteSettings settings;
+  final MorphBloc morphBloc;
+
+  RouteGenerator(this.settings, this.morphBloc);
+
+  Route<dynamic>? generate() {
+    final Object? args = settings.arguments;
+
+    switch (settings.name) {
+      case MorphRoute.main:
+        return MaterialPageRoute(
+          builder: (_) => const MainScreen(),
+        );
+      case MorphRoute.onboarding:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case MorphRoute.createWallet:
+        return MaterialPageRoute(
+          builder: (_) => CreateWallet(mnemonic: args as List<String>),
+        );
+      case MorphRoute.walletInfoForm:
+        return MaterialPageRoute(builder: (_) => const WalletInfoFormScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => const EmptyScreen());
+    }
+  }
+}
