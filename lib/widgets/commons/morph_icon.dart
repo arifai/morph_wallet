91e40a9a25a14d0ab579f15a455f0bcb30bf538d
Ahmad Rifa'i <arifai209@gmail.com>
2022-04-11 20:14:44 +0700

    Create MorphIcon widget

---
+++ lib/widgets/commons/morph_icon.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/widgets.dart';
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: constant_identifier_names
+@immutable
+class _MorphIconData extends IconData {
+  const _MorphIconData(int codePoint, this.name)
+      : super(
+          codePoint,
+          fontFamily: 'MorphIcon',
+        );
+
+  final String name;
+}
+
+@immutable
+class MorphIcon {
+  const MorphIcon._();
+
+  static const arrow_swap =
+      _MorphIconData(0xf101, 'fluent_arrow_swap24_filled');
+  static const book_coin = _MorphIconData(0xf102, 'fluent_book_coins24_filled');
+  static const checkmark_circle =
+      _MorphIconData(0xf103, 'fluent_checkmark_circle32_filled');
+  static const document_multiple =
+      _MorphIconData(0xf104, 'fluent_document_multiple24_filled');
+  static const globe = _MorphIconData(0xf105, 'fluent_globe32_filled');
+  static const scan = _MorphIconData(0xf106, 'fluent_scan_dash24_filled');
+  static const search = _MorphIconData(0xf107, 'fluent_search12_filled');
+  static const settings = _MorphIconData(0xf108, 'fluent_settings32_filled');
+  static const wallet = _MorphIconData(0xf109, 'fluent_wallet32_filled');
+
+  static const all = <String, _MorphIconData>{
+    'fluent_arrow_swap24_filled': arrow_swap,
+    'fluent_book_coins24_filled': book_coin,
+    'fluent_checkmark_circle32_filled': checkmark_circle,
+    'fluent_document_multiple24_filled': document_multiple,
+    'fluent_globe32_filled': globe,
+    'fluent_scan_dash24_filled': scan,
+    'fluent_search12_filled': search,
+    'fluent_settings32_filled': settings,
+    'fluent_wallet32_filled': wallet,
+  };
+}
